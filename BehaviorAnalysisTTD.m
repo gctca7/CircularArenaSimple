f5a14fe1ddb2ac79a26bad98857e51646db42109
Glenn <glenncturner@gmail.com>
2020-11-28 15:46:54 -0500

    Minor

--- BehaviorAnalysisTTD.m
+++ BehaviorAnalysisTTD.m
@@ -5,27 +5,20 @@ X Pass FrameRate to FlySpotter
 X PassBackgroundImage and ArenaEdge to FlySpotter
 Regularize all terms across functions
 X Would masking be faster as a matrix operation?
-
 X Interactively place the arena border
 X Adjust so it matches ArenaX CamX
 X Test how accurate FlySpotter is by plotting 6 or 8 random frames (not binarized!) with centroid markers
 Test how much faster FlySpotter is if you just collect Centroids (and if you do it NOT in a structure)
-
 X Have a QC where you plot Flyspots for the frames with lower number total flies
 X Also plot the circle used for Arena and quadrant markers.
-
 X What information do you want to pass from ArenaEdgesnBackground
     X Should rename function as CirclePrep.m or ArenaSetup (what did Yichun do?)
         X video header info (incl timestamps)
         X Arena & Camera
         X BackgroundImage & ArenaEdge
-        
-What is best directory arrangement for analyzed flies?
-    Do you want to use the analyzed.txt tag or is it annoying to have to delete when you reanalyze?
-Check if subpixel resolution is working with inpolygon using a test fly centroid with fractional location
 X Change Start Stop to Dur = [t1 t2] & Dur(1) for start Dur(2) for stop 
+X Plot quadrants on the arena as part of ArenaSetup
+Check if subpixel resolution is working with inpolygon using a test fly centroid with fractional location
 Find pretty way of plotting halfPI and PI and FlyCount Timecourse (thicken line for PI count - patch for odor)
 Finish PICalcaultor with plotting of reciprocal PIs.
-X Plot quadrants on the arena as part of ArenaSetup
-
 Save PI vs time plots so you don't have to crop them.
--- README.md
+++ README.md
@@ -5,14 +5,14 @@ ArenaInfo = ArenaSetup() ;
     Determines ArenaEdge and calculates BackgroundImage of arena separately for Cam0 and Cam1
 
 FlySpotter.m
-    Locates flies centroids and returns 
-    Frame.FlySpots.Centroid
+    Locates flies centroids and returns. 
+		Frame.FlySpots.Centroid
 
 FlyCounter.m
-    Counts flies in each quadrant and returns
-    FlyCount(Frame,Quadrant)
+    Counts flies in each quadrant and returns  
+		FlyCount(Frame,Quadrant)
 
 PICalculator.m
-    Calculates PIs based on FlyCount and returns
-    PI timecourse
-    half PI i.e. PI for one odor
+    Calculates PIs based on FlyCount and returns  
+		i) PI timecourse  
+		ii) half PI i.e. PI for one odor
